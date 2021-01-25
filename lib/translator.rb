02cde55fe2e1075373993e87b4af0d6c4db5c00f
Mohamed Hossain <mz.hossain212@gmail.com>
2021-01-25 23:04:36 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,8 +1,13 @@
 # require modules here
 
-def load_library
-  # code goes here
+def load_library(path)
+  new_hash = {}
+  YAML.load_file(path).each do |key, value|
+  new_hash[key] = {}
+  new_hash[key][:english] = value[0]
+  new_hash[key][:japanese] = value[1]
 end
+  new_hash
 
 def get_japanese_emoticon
   # code goes here
